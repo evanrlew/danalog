4f1be79237ef08b4ddc5d4f6e6fc2c4a789c653f
evanrlew <evan.lew.com@gmail.com>
2017-05-27 01:49:56 -0700

    slow down spi

--- spi_config.c
+++ spi_config.c
@@ -8,7 +8,7 @@
 #include "spi_config.h"
 #include "csl_gpio.h"
 
-#define SPI_RW_WAIT 25
+#define SPI_RW_WAIT 200
 
 SPI_Config		spi_hwConfig;
 
@@ -38,7 +38,7 @@ void spi_init( void ) {
 
 	hSpi = SPI_open(SPI_CS_NUM_1, SPI_POLLING_MODE);
 
-	spi_hwConfig.spiClkDiv	= 25;
+	spi_hwConfig.spiClkDiv	= 100;
 	spi_hwConfig.wLen		= SPI_WORD_LENGTH_8;
 	spi_hwConfig.frLen		= 1;
 	spi_hwConfig.wcEnable	= SPI_WORD_IRQ_DISABLE;
