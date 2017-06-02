491cf7f6b8e841fd967c2321a8daa855487b72f9
evanrlew <evan.lew.com@gmail.com>
2017-06-02 02:37:25 -0700

    not working

--- eq.c
+++ eq.c
@@ -9,12 +9,17 @@
 /* Standard C includes */
 #include <std.h>
 #include <stdio.h>
+#include <math.h>
 #include <tsk.h>
+#include "Dsplib.h"
 #include "audio/singen.h"
 #include "global_vars.h"
+#include "TMS320.H"
 
 #pragma DATA_SECTION(Buffer, "Buffer");
 Int16 Buffer[256];
+//#pragma DATA_SECTION(complex_data,"complex_data");
+DATA complex_data[512];
 
 
 Void eq_tsk( Void ) {
@@ -27,19 +32,20 @@ Void eq_tsk( Void ) {
 		for (i = 0; i < 256; ++i) {
 			Buffer[i] = sin_gen(&EQSine, 0);
 		}
-		Uint8 Knob_Values[5] = {pots[0], pots[1], pots[2], pots[3], pots[4]};
-		printf("%d %d %d %d %d\n", pots[0], pots[1], pots[2], pots[3], pots[4]);
-		Uint8 EqKIndex[5] = {1, 3, 9, 27, 81};
-		Uint8 Interval_Lengths[4] = {2, 6, 18, 54};
-		Int8 EqualizerFD[256];
+//		Uint8 Knob_Values[5] = {pots[0], pots[1], pots[2], pots[3], pots[4]};
+		Int16 Knob_Values[5] = {1, 10, 20, 40, 30};
+		//printf("%d %d %d %d %d\n", Knob_Values[0], Knob_Values[1], Knob_Values[2], Knob_Values[3], Knob_Values[4]);
+		Int16 EqKIndex[5] = {1, 3, 9, 27, 81};
+		Int16 Interval_Lengths[4] = {2, 6, 18, 54};
+		Int16 EqualizerFD[256];
 		EqualizerFD[0] = Knob_Values[0];
-		//printf("Equalizer Values\n%d,",EqualizerFD[0]);
+		//printf("\n%i,",EqualizerFD[0]);
 		EqualizerFD[1] = Knob_Values[0];
-		//printf("%d,",EqualizerFD[1]);
-		Uint8 l;
-		Uint8 m;
-		Uint8 n;
-		Int8 Interp_M;
+		//printf("%i,",EqualizerFD[1]);
+		Int16 l;
+		Int16 m;
+		Int16 n;
+		Int16 Interp_M;
 		for(l = 0; l < 4; l++) {
 		//make array
 		   	n = 1;
@@ -47,23 +53,31 @@ Void eq_tsk( Void ) {
 		   	for(m = EqKIndex[l]+1; m < EqKIndex[l+1]; m++) {
 		   		EqualizerFD[m] = Knob_Values[l] + Interp_M * n;
 		   		n++;
-//		   		printf("%d,",EqualizerFD[m]);
+		   		//printf("%i,",EqualizerFD[m]);
 		   	}
 		   	EqualizerFD[m] = Knob_Values[l+1];
-//		   	printf("%d,",EqualizerFD[m]);
+		   	//printf("%i,",EqualizerFD[m]);
 		}
 		for(l = 82; l < 128; l++) {
 		   	EqualizerFD[l] = Knob_Values[4];
-//		    printf("%d,",EqualizerFD[l]);
+		    //printf("%i,",EqualizerFD[l]);
 		}
 		for(i = 0;i < 128; i++) {
 		   	EqualizerFD[255-i] = EqualizerFD[i];
-//		   	printf("%d, ",EqualizerFD[128+i]);
+		   	//printf("%i,",EqualizerFD[128+i]);
 		}
-
+		printf("\n");
+		for(i=0;i<256;i++) {
+			complex_data[2*i] = EqualizerFD[i];
+			complex_data[2*i+1] = 0;
+			//printf("%d, ", complex_data[2*i]);
+		}
+		for(i=0;i<512;i++) {
+			printf("%d, ", complex_data[i]);
+		}
+		cifft_SCALE(complex_data, 256)
 		//cfft_SCALE(complex_data, 256);
 		//cbrev(complex_data, complex_data, 256);
 		TSK_sleep(100);
 	}
 }
-
