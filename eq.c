c8420800741458c84ea0339cdd1c275435c76735
evanrlew <evan.lew.com@gmail.com>
2017-06-02 14:59:16 -0700

    checkpoint

--- eq.c
+++ eq.c
@@ -61,48 +61,11 @@ Void eq_tsk( Void ) {
 //	}
 	while(1) {
 		// eq code
-		for (i = 0; i < 256; ++i) {
-			Buffer[i] = sin_gen(&EQSine, 0);
-		}
-//		Uint8 Knob_Values[5] = {pots[0], pots[1], pots[2], pots[3], pots[4]};
-		Int16 Knob_Values[5] = {1, 10, 20, 40, 30};
-		//printf("%d %d %d %d %d\n", Knob_Values[0], Knob_Values[1], Knob_Values[2], Knob_Values[3], Knob_Values[4]);
-		Int16 EqKIndex[5] = {1, 3, 9, 27, 81};
-		Int16 Interval_Lengths[4] = {2, 6, 18, 54};
-		Int16 EqualizerFD[256];
-		EqualizerFD[0] = Knob_Values[0];
-		//printf("\n%i,",EqualizerFD[0]);
-		EqualizerFD[1] = Knob_Values[0];
-		//printf("%i,",EqualizerFD[1]);
-		Int16 l;
-		Int16 m;
-		Int16 n;
-		Int16 Interp_M;
-		for(l = 0; l < 4; l++) {
-		//make array
-		   	n = 1;
-		   	Interp_M = (Knob_Values[l+1] - Knob_Values[l]) / Interval_Lengths[l];
-		   	for(m = EqKIndex[l]+1; m < EqKIndex[l+1]; m++) {
-		   		EqualizerFD[m] = Knob_Values[l] + Interp_M * n;
-		   		n++;
-		   		//printf("%i,",EqualizerFD[m]);
-		   	}
-		   	EqualizerFD[m] = Knob_Values[l+1];
-		   	//printf("%i,",EqualizerFD[m]);
-		}
-		for(l = 82; l < 128; l++) {
-		   	EqualizerFD[l] = Knob_Values[4];
-		    //printf("%i,",EqualizerFD[l]);
-		}
-		for(i = 0;i < 128; i++) {
-		   	EqualizerFD[255-i] = EqualizerFD[i];
-		   	//printf("%i,",EqualizerFD[128+i]);
-		}
-		printf("\n");
-		for(i=0;i<256;i++) {
-			complex_data[2*i] = EqualizerFD[i];
-			complex_data[2*i+1] = 0;
-			//printf("%d, ", complex_data[2*i]);
+		printf("\nSine Wave ");
+		for (i = 0; i < 128; ++i) {
+			x[i] = sin_gen(&EQSine, 0);
+
+			printf("%i,", x[i]);
 		}
 
 		printf("\nFilter ");
