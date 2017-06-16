f17cf069b48838999c0a39fec45bf6afbe7dcf35
evanrlew <evan.lew.com@gmail.com>
2017-06-15 21:48:58 -0700

    Setting up ping and pong transfer now

--- eq.c
+++ eq.c
@@ -33,7 +33,8 @@ Int32 Ak4[3] = {10000, -10880, 4761};
 Int32 Bk4[3] = {2618, 0, -2618};
 Int32 Ak5[3] = {10000, 4782, 3481};
 Int32 Bk5[3] = {3257, 0, -3257};
-Int32 y1[128];
+Int32 y1[128] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+Int32 Next_y1[128];
 Int32 y2[128];
 Int32 y3[128];
 Int32 y4[128];
@@ -66,37 +67,46 @@ Void eq_tsk( Void ) {
 	SinState EQSine;
 	sin_compute_params(&EQSine, 6000);
 	Int16 i;
+	for (i = 0; i < 128; ++i) {
+		x[i] = sin_gen(&EQSine, 0);
+	}
+	Next_y1[0] = Bk1[0]*x[0]/Ak1[0];
+	Next_y1[1] = (Bk1[0]*x[1]+Bk1[1]*x[0]-Ak1[1]*y1[0])/Ak1[0];
+	Next_y1[2] = (Bk1[0]*x[2]+Bk1[1]*x[1]+Bk1[2]*x[0]-Ak1[1]*y1[1]-Ak1[2]*y1[0])/Ak1[0];
 	while(1) {
-		// eq code
-		//printf("\nSine Wave ");
-		for (i = 0; i < 128; ++i) {
-			x[i] = sin_gen(&EQSine, 0);
-		//	printf("%d,", x[i]);
+//		Ak1[i]*y1[i]+Ak1[i-1]*y1[i-1]+Ak1[i-2]*y1[i-2] = Bk1[i]*x[i]+Bk1[i-1]*x[i-1]+Bk1[i-2]*x[i-2]
+//		y1[i] = (Bk1[0]*x[i]+Bk1[1]*x[i-1]+Bk1[2]*x[i-2]-Ak1[1]*y1[i-1]-Ak1[2]*y1[i-2])/Ak1[0];
+		y1[0] = Next_y1[0];
+		y1[1] = Next_y1[1];
+		y1[2] = Next_y1[2];
+		for (i = 3; i < 128; i++) {
+			y1[i] = (Bk1[0]*x[i]+Bk1[1]*x[i-1]+Bk1[2]*x[i-2]-Ak1[1]*y1[i-1]-Ak1[2]*y1[i-2])/Ak1[0];
 		}
-
-		//printf("\nFilter ");
-		First_Term = Bk[0]*x[0];
-		y[0] = First_Term/Ak;
-		First_Term = Bk[0]*x[1];
-		Second_Term = Bk[1]*x[0];
-		Third_Term = 0;
-		y[1] = First_Term + Second_Term;
-		y[1] = y[1]/Ak;
-		First_Term = Bk[0]*x[2];
-		Second_Term = Bk[1]*x[1];
-		Third_Term = Bk[2]*x[0];
-		y[2] = First_Term + Second_Term + Third_Term;
-		y[2] = y[2]/Ak;
-
-		for (var = 3; var < 128; ++var) {
-			First_Term = Bk[0]*x[var];
-			Second_Term = Bk[1]*x[var-1];
-			Third_Term = Bk[2]*x[var-2];
-			y[var] = First_Term + Second_Term + Third_Term;
-			y[var] = y[var]/Ak;
-//			y[i] = (Bk[0]*x[i] + Bk[1]*x[i-1] + Bk[2]*x[i-2])/Ak;
-			//printf("%d,", y[i]);
-		}
+		Next_y1[0] = y1[125];
+		Next_y1[1] = y1[126];
+		Next_y1[2] = y1[127];
+//		First_Term = Bk[0]*x[0];
+//		y[0] = First_Term/Ak;
+//		First_Term = Bk[0]*x[1];
+//		Second_Term = Bk[1]*x[0];
+//		Third_Term = 0;
+//		y[1] = First_Term + Second_Term;
+//		y[1] = y[1]/Ak;
+//		First_Term = Bk[0]*x[2];
+//		Second_Term = Bk[1]*x[1];
+//		Third_Term = Bk[2]*x[0];
+//		y[2] = First_Term + Second_Term + Third_Term;
+//		y[2] = y[2]/Ak;
+//
+//		for (var = 3; var < 128; ++var) {
+//			First_Term = Bk[0]*x[var];
+//			Second_Term = Bk[1]*x[var-1];
+//			Third_Term = Bk[2]*x[var-2];
+//			y[var] = First_Term + Second_Term + Third_Term;
+//			y[var] = y[var]/Ak;
+////			y[i] = (Bk[0]*x[i] + Bk[1]*x[i-1] + Bk[2]*x[i-2])/Ak;
+//			//printf("%d,", y[i]);
+//		}
 
 ////		Uint8 Knob_Values[5] = {pots[0], pots[1], pots[2], pots[3], pots[4]};
 //		Int16 Knob_Values[5] = {1, 10, 20, 40, 30};
